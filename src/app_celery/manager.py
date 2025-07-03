91ecdb0f47229f4c022ce409eb26b730e2a98bfb
Egor <egormeschenko@gmail.com>
2025-07-03 16:14:18 +0300

    add redis db for this project

--- src/app_celery/manager.py
+++ src/app_celery/manager.py
@@ -1,31 +1,38 @@
+import json
+
+from redis import Redis
+
 from src.app_celery.main import app
 from celery.result import AsyncResult
 from src.app_celery.tasks import parse_api
 import datetime
-
+rds = Redis()
+rds.set('N', 3)
 # Сохраняем ID текущих задач — в реальном проекте лучше использовать БД или Redis
 running_tasks = {}
 
 @app.task
 def manager_task():
     global running_tasks
-
-    print(f"[{datetime.datetime.now()}] Менеджер запущен")
+    n = int(rds.get('N'))
+    elem = json.loads(rds.spop('task_set'))
+    print(f"[{datetime.datetime.now()}] Manager is running")
 
     # Удалим завершённые задачи из слежения
     finished = [tid for tid, r in running_tasks.items() if AsyncResult(tid).ready()]
     for tid in finished:
-        print(f"Задача {tid} завершена")
+        print(f"task finished {tid}")
         running_tasks.pop(tid)
+        elem = json.loads(rds.spop('task_set'))
+        result = parse_api.delay(elem)
+        running_tasks[result.id] = result
+        print(f"Running new task: {result.id}")
 
     if running_tasks:
-        print("Есть незавершённые задачи. Ждём следующего запуска.")
         return
 
-    # Здесь — логика генерации новых заданий
-    for i in range(3):  # например, три новых запроса
-        url = "https://api.example.com/data"
-        params = {"q": f"item{i}", "page": 1}
-        result = parse_api.delay(url, params)
+    for i in range(n):
+        elem = json.loads(rds.spop('task_set'))
+        result = parse_api.delay(elem)
         running_tasks[result.id] = result
-        print(f"Запущена новая задача: {result.id}")
+        print(f"Running new task: {result.id}")
